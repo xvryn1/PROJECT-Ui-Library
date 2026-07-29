4beb759dee240e9306e36cca240c35f09ef234c5
xvryn1 <alfiedaffa5@gmail.com>
2026-07-30 06:07:12 +0700

    Initialize AtomicUI framework in init.lua

---
+++ init.lua
@@ -0,0 +1,15 @@
+--!strict
+-- AtomicUI v1.0.0
+-- Production-ready Roblox UI Framework
+
+local AtomicUI = {
+	Version = "1.0.0",
+	Theme = nil,
+	Utility = nil,
+	Animation = nil,
+	IconManager = nil,
+	Core = {},
+	Components = {},
+}
+
+return AtomicUI
